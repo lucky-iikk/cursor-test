2449abea30b53e22e1e304f852aaaa9aee8b115c
glasses <greatyi@icloud.com>
2025-04-17 11:19:23 +0800

    代码修改

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -3,11 +3,13 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "hello world 3.0 未来可期";
+        return "hello world 3.0 未来可期 " + new Date();
     }
 } 
